3ff29058aa67a98c73a617918202b80457265cc4
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-03 03:46:51 +0300

    Initialize Voidstrap MVP with basic WPF UI framework

---
+++ VoidstrapMVP/App.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+
+namespace VoidstrapMVP
+{
+    public partial class App : Application
+    {
+        public const string ProjectName = "VoidstrapMVP";
+        public const string Version = "1.0.0";
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+    }
+}
